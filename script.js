dc921628278b8b0609779615ba83fb0975114192
Mugesh Kumar K <105528383+Mugesh2297@users.noreply.github.com>
2022-07-28 11:58:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let div = document.querySelector("#container");
+let h2 = document.createElement("h2");
+h2.setAttribute("class", "col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12");
+div.append(h2);
+h2.textContent = "10";
+
+
+  setTimeout(() => {
+    h2.textContent = "9";
+    setTimeout(() => {
+      h2.textContent = "8";
+      setTimeout(() => {
+        h2.textContent = "7";
+        setTimeout(() => {
+          h2.textContent = "6";
+          setTimeout(() => {
+            h2.textContent = "5";
+            setTimeout(() => {
+              h2.textContent = "4";
+              setTimeout(() => {
+                h2.textContent = "3";
+                setTimeout(() => {
+                  h2.textContent = "2";
+                  setTimeout(() => {
+                    h2.textContent = "1";
+                    setTimeout(() => {
+                      h2.textContent = "Happy Independence Day";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
